e4a1f318f5883cf90475fd25678cb13859acaac1
カグチ <59391215+KagChi@users.noreply.github.com>
2021-08-27 12:11:34 +0700

    few fix

--- src/Plugin.ts
+++ src/Plugin.ts
@@ -81,6 +81,8 @@ export class persistentQueue extends Plugin {
                 for (let track of db.queue) {
                     player.queue.add(TrackUtils.buildUnresolved({ title: track.title, author: track.author, duration: track.duration }, new User(client, db.current.requester)))
                 }
+                player.setTrackRepeat(db.trackRepeat ?? false);
+                player.setQueueRepeat(db.queueRepeat ?? false)
                 player.play(TrackUtils.buildUnresolved(player.queue.current!, new User(client, db.current.requester)), { startTime: db.position ?? 0 });
             }
         })
